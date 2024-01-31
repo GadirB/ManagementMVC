f9d37018363c41ac2a7ee8dd44e6245c53740b16
qadir.babayev <qadir.babayev@setclapp.com>
2024-01-31 14:37:47 +0400

    Add project files.

---
+++ ManagementMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManagementMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
